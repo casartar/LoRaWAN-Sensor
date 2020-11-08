c08b87f44d3bcf2c5c0a3334500b449b2d6fa7d7
Peter Scheydt <casartar@web.de>
2020-11-08 21:38:59 +0100

    curly brackets removed

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -54,17 +54,17 @@
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-static const u1_t PROGMEM APPEUI[8]={ DEF_APPEUI };
+static const u1_t PROGMEM APPEUI[8] = DEF_APPEUI;
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]={ DEF_DEVEUI };
+static const u1_t PROGMEM DEVEUI[8] = DEF_DEVEUI;
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
-static const u1_t PROGMEM APPKEY[16] = { DEF_APPKEY };
+static const u1_t PROGMEM APPKEY[16] = DEF_APPKEY;
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
 static osjob_t sendjob;
